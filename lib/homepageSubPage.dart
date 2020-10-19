cdcf14414b8b7d19d7e416b3ad5526ef58357e65
apple <18201497620@163.com>
2020-10-20 00:48:19 +0800

    首页subfragment列表数据加入

--- lib/homepageSubPage.dart
+++ lib/homepageSubPage.dart
@@ -5,7 +5,29 @@ class HomepageSubpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 0,
+      child: HomeContent(),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  List<String> list = new List();
+
+  HomeContent() {
+    for(var i=0;i<20;i++) {
+      list.add("第$i条数据");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(list[index]),
+        );
+      },
     );
   }
 
--- lib/pageContent.dart
+++ lib/pageContent.dart
@@ -13,13 +13,13 @@ class PageContent extends StatefulWidget {
 
 class PageContentState extends State<PageContent>
     with SingleTickerProviderStateMixin {
-  List tabTitle = ["关注", "推荐", "热榜", "要闻"];
+  List tabs = ["关注", "推荐", "热榜", "要闻", "新时代", "抗疫"];
   TabController tabController;
 
   @override
   void initState() {
     super.initState();
-    tabController = new TabController(length: tabTitle.length, vsync: this);
+    tabController = new TabController(length: tabs.length, vsync: this);
     tabController.addListener(() {
       var index = tabController.index;
     });
@@ -29,19 +29,25 @@ class PageContentState extends State<PageContent>
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.red,
           bottom: TabBar(
             controller: tabController,
+            isScrollable: true,
             tabs: <Widget>[
               Tab(text: "关注"),
               Tab(text: "推荐"),
               Tab(text: "热榜"),
               Tab(text: "要闻"),
+              Tab(text: "新时代"),
+              Tab(text: "抗疫"),
             ],
           ),
         ),
         body: TabBarView(
             controller: tabController,
             children: <Widget>[
+              HomepageSubpage(),
+              HomepageSubpage(),
               HomepageSubpage(),
               HomepageSubpage(),
               HomepageSubpage(),
